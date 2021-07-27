56fa69770c9407749a0a720b6f938d828601e52e
Maris Botero <maris2143@gmail.com>
2021-07-27 08:45:15 -0500

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,23 @@
+#########################################################
+## File Name: hangman.py                               ##
+## Description: Starter for Hangman project            ##
+#########################################################
+
+
+import pygame
+import random
+
+pygame.init()
+ygame.init()
+winHeight = 480
+winWidth = 700
+win=pygame.display.set_mode((winWidth,winHeight))
+#---------------------------------------#
+# initialize global variables/constants #
+#---------------------------------------#
+BLACK = (0,0, 0)
+WHITE = (255,255,255)
+RED = (255,0, 0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+LIGHT_BLUE = (102,255,255)
